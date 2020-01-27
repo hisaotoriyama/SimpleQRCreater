20b83dedcdd50ba68400be66827a04d7b4f2e6b6
hisaotoriyama <hisao.toriyama@gmail.com>
2020-01-27 20:59:18 +0900

    20200127

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // setup application
-let express = require('express')
-let Res = require('express-resource')
-let app = express()
+let express = require('express');
+let Res = require('express-resource');
+let app = express();
+let path = require('path');
 
 app.use('/', express.static(path.join( __dirname, '/public')));
 // register REST controllers
